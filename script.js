baa8cf0f3d34848ddff78bed43fe287e81cf3ac4
alexdenuke <alexdenuke@gmail.com>
2022-06-14 13:08:44 +0300

    optimization burger menu

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-let open = document.querySelector('.burger');
+let open = document.querySelector('.burger-open');
 let menu = document.querySelector('.burger-main');
 let close = document.querySelector('.burger-close')
 
 open.addEventListener('click', function () {
-   menu.style.top = 0 + "vh";
+   menu.style.top =  "calc(0vh + 120px)";
+   open.style.display = "none";
+   close.style.display = "block";
    document.body.style.overflow = 'hidden';  
 })
 
 close.addEventListener('click', function () {
-    menu.style.top = -100 + "vh"; 
+    menu.style.top = "-100vh";
+    close.style.display = "none";
+    open.style.display = "block";
     document.body.style.overflow = 'visible';
  })
