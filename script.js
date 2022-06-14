0497cbad6e3856ef316085ef248306d1ba4b061e
alexdenuke <alexdenuke@gmail.com>
2022-06-14 15:52:27 +0300

    start popup

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-let open = document.querySelector('.burger-open');
-let menu = document.querySelector('.burger-main');
-let close = document.querySelector('.burger-close')
+let open = document.querySelector(".burger-open");
+let menu = document.querySelector(".burger");
+let wrap = document.querySelector(".header__button-wrap");
+let header = document.querySelector('.header__nav-wrap');
+let computedStyle = getComputedStyle(header);
+let height = computedStyle.height;
+let img = document.createElement("img");
+img.src = "/img/burger-close.svg";
+img.classList.add("burger-close");
+let close = img;
 
-open.addEventListener('click', function () {
-   menu.style.top =  "calc(0vh + 120px)";
-   open.style.display = "none";
-   close.style.display = "block";
-   document.body.style.overflow = 'hidden';  
-})
+open.addEventListener("click", function () {
+  menu.style.top = `calc(0vh + ${height})`;
+  open.remove();
+  wrap.after(img);
+  // close.style.display = "block";
+  document.body.style.overflow = "hidden";
+});
 
-close.addEventListener('click', function () {
-    menu.style.top = "-100vh";
-    close.style.display = "none";
-    open.style.display = "block";
-    document.body.style.overflow = 'visible';
- })
+close.addEventListener("click", function () {
+  menu.style.top = "-100vh";
+  close.remove();
+  wrap.after(open);  
+  document.body.style.overflow = "visible";
+});
